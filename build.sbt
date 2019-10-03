8451f59089dbba7540e6e2398e9c35d23c658f2c
Shyam Patel <spate54@uic.edu>
2019-10-03 18:46:48 -0500

    Switched to ScalaTest testing framework

--- build.sbt
+++ build.sbt
@@ -13,14 +13,10 @@ scalaVersion := "2.13.1"
 libraryDependencies ++= Seq(
   // Logback Classic Module
   "ch.qos.logback" % "logback-classic" % "1.2.3",
-  // Logback Core Module
-  "ch.qos.logback" % "logback-core" % "1.2.3",
   // Typesafe Config
   "com.typesafe" % "config" % "1.3.4",
-  // Apache Commons Math
-  "org.apache.commons" % "commons-math3" % "3.6.1",
-  // JUnit Jupiter API
-  "org.junit.jupiter" % "junit-jupiter-api" % "5.5.2",
+  // ScalaTest
+  "org.scalatest" %% "scalatest" % "3.0.8" % "test",
   // SLF4J API Module
   "org.slf4j" % "slf4j-api" % "1.7.28"
 )
--- src/test/scala/SimulationTests.scala
+++ src/test/scala/SimulationTests.scala
@@ -7,275 +7,147 @@
 import com.hw1.dc.{MyBroker, MyCloudlet, MyDatacenter, MyHost, MyVm}
 import com.typesafe.config.{Config, ConfigFactory}
 import org.cloudbus.cloudsim.core.CloudSim
-import org.cloudbus.cloudsim.{Cloudlet, Datacenter, DatacenterBroker, Host, Log, Vm}
-import org.junit.jupiter.api.{BeforeEach, DisplayName, Test}
-import org.junit.jupiter.api.Assertions.{assertEquals, assertFalse, assertNotNull, assertTrue}
-import org.slf4j.{Logger, LoggerFactory}
+import org.cloudbus.cloudsim.{Cloudlet, DatacenterBroker, Log}
+import org.scalatest.{FlatSpec, Matchers}
 import java.util.Calendar
 import java.util
 
-// SimulationTests class consisting of 8 unit tests based on the JUnit test framework
-@DisplayName("Simulation Tests")
-class SimulationTests {
-  // logger
-  val log: Logger = LoggerFactory.getLogger(this.getClass)
+// SimulationTests class consisting of 8 unit tests based on the ScalaTest testing framework
+class SimulationTests extends FlatSpec with Matchers {
   // configuration
   val conf: Config = ConfigFactory.load("simulation3")
 
-  // TEST: initialize CloudSim
-  @BeforeEach @Test @DisplayName("CloudSim Initialization")
-  def initialization(): Unit = {
-    assertNotNull(log)
-    assertNotNull(conf)
-    val num_user: Int = conf.getInt("simulation3.num_user")
-    val trace_flag: Boolean = conf.getBoolean("simulation3.trace_flag")
-    assertNotNull(num_user)
-    assertNotNull(trace_flag)
-    assertEquals(1, num_user)
-    assertFalse(trace_flag)
-    CloudSim.init(num_user, Calendar.getInstance, trace_flag)
+  // TEST 1: initialize CloudSim
+  "CloudSim Initialization" should "be successful" in {
+    conf.getInt("simulation3.num_user") should be (1)
+    conf.getBoolean("simulation3.trace_flag") should be (false)
+    CloudSim.init(1, Calendar.getInstance, false)
     Log.disable()
-    val entityId: Int = CloudSim.getCloudInfoServiceEntityId
-    assertNotNull(entityId)
-    assertEquals(1, entityId)
-  }//end def initialization
+    CloudSim.getCloudInfoServiceEntityId should be (1)
+  }
 
-  // TEST: load configuration and read from it
-  @Test @DisplayName("Loading Configuration")
-  def configuration(): Unit = {
-    log.info("*** TEST: Loading Configuration ***")
-    val c: Config = ConfigFactory.load("simulation1")
-    log.info("Loading configuration...")
-    assertNotNull(c)
-    val num_user: Int = c.getInt("simulation1.num_user")
-    val trace_flag: Boolean = c.getBoolean("simulation1.trace_flag")
-    val map_reduce: Boolean = c.getBoolean("simulation1.map_reduce")
-    val num_datacenter: Int = c.getInt("simulation1.num_datacenter")
-    val num_broker: Int = c.getInt("simulation1.num_broker")
-    val os: String = c.getString("simulation1.datacenter1.os")
-    val time_zone: Double = c.getDouble("simulation1.datacenter1.time_zone")
-    val costPerMem: Double = c.getDouble("simulation1.datacenter1.costPerMem")
-    val ram: Int = c.getInt("simulation1.datacenter1.host1.ram")
-    val mips: Int = c.getInt("simulation1.datacenter1.host1.mips")
-    assertNotNull(num_user)
-    assertNotNull(trace_flag)
-    assertNotNull(map_reduce)
-    assertNotNull(num_datacenter)
-    assertNotNull(num_broker)
-    assertNotNull(os)
-    assertNotNull(time_zone)
-    assertNotNull(costPerMem)
-    assertNotNull(ram)
-    assertNotNull(mips)
-    log.info(s"Number of users: $num_user (expected 1)")
-    log.info(s"Trace events: $trace_flag (expected false)")
-    log.info(s"MapReduce: $map_reduce (expected false)")
-    log.info(s"Number of datacenters: $num_datacenter (expected 1)")
-    log.info(s"Number of brokers: $num_broker (expected 2)")
-    log.info(s"OS of datacenter1: $os (expected Linux)")
-    log.info(s"Time zone of datacenter1: UTC$time_zone (expected UTC-5.0)")
-    log.info(s"Memory cost of datacenter1: $$$costPerMem (expected $$0.05)")
-    log.info(s"Memory of host1: $ram MB (expected 2048 MB)")
-    log.info(s"MIPS rating of host1: $mips mn instructions/sec (expected 3000)")
-    assertEquals(1, num_user)
-    assertFalse(trace_flag)
-    assertFalse(map_reduce)
-    assertEquals(1, num_datacenter)
-    assertEquals(2, num_broker)
-    assertEquals("Linux", os)
-    assertEquals(-5.0, time_zone)
-    assertEquals(0.05, costPerMem)
-    assertEquals(2048, ram)
-    assertEquals(3000, mips)
-    log.info("Test successfully completed.")
-  }//end def configuration
+  // TEST 2: load configuration and read from it
+  "Datacenter Configuration" should "have 1 user" in {
+    ConfigFactory.load("simulation1").getInt("simulation1.num_user") should be (1)
+  }
+  it should "have trace_flag disabled" in {
+    ConfigFactory.load("simulation1").getBoolean("simulation1.trace_flag") should be (false)
+  }
+  it should "have map_reduce disabled" in {
+    ConfigFactory.load("simulation1").getBoolean("simulation1.map_reduce") should be (false)
+  }
+  it should "have 1 datacenter" in {
+    ConfigFactory.load("simulation1").getInt("simulation1.num_datacenter") should be (1)
+  }
+  it should "have 2 brokers" in {
+    ConfigFactory.load("simulation1").getInt("simulation1.num_broker") should be (2)
+  }
+  it should "have Linux OS" in {
+    ConfigFactory.load("simulation1").getString("simulation1.datacenter1.os") should be ("Linux")
+  }
+  it should "have UTC-5:00 time" in {
+    ConfigFactory.load("simulation1").getDouble("simulation1.datacenter1.time_zone") should be (-5.0)
+  }
+  it should "have $0.05 cost per memory" in {
+    ConfigFactory.load("simulation1").getDouble("simulation1.datacenter1.costPerMem") should be (0.05)
+  }
+  it should "have host1 with 2 GB RAM" in {
+    ConfigFactory.load("simulation1").getInt("simulation1.datacenter1.host1.ram") should be (2048)
+  }
+  it should "have host1 with 3000 MIPS" in {
+    ConfigFactory.load("simulation1").getInt("simulation1.datacenter1.host1.mips") should be (3000)
+  }
 
-  // TEST: create datacenter with hosts
-  @Test @DisplayName("Datacenter Creation")
-  def createDatacenter(): Unit = {
-    log.info("*** TEST: Datacenter Creation ***")
-    val num_datacenter: Int = conf.getInt("simulation3.num_datacenter")
-    assertNotNull(num_datacenter)
-    assertEquals(1, num_datacenter)
-    log.info("Creating datacenter...")
-    log.info("Creating hosts...")
-    val datacenters: List[Datacenter] = List.tabulate(num_datacenter)(n =>
-      MyDatacenter.create(n + 1, 3, "simulation3", conf))
-    assertNotNull(datacenters)
-    assertEquals(1, datacenters.size)
-    val name: String = datacenters(0).getName
-    assertNotNull(name)
-    assertEquals("datacenter1", name)
-    log.info(s"$name successfully created.")
-    val hosts: util.List[_ <: Host] = datacenters(0).getHostList
-    assertNotNull(hosts)
-    assertEquals(2, hosts.size)
-    hosts.forEach(host => log.info(String.format("host%d successfully created.", host.getId)))
-    log.info("Test successfully completed.")
-  }//end def createDatacenter
+  // TEST 3: create datacenter with hosts
+  "Datacenter" should "be the only one" in {
+    conf.getInt("simulation3.num_datacenter") should be (1)
+  }
+  it should "be named datacenter1" in {
+    List.tabulate(1)(n => MyDatacenter.create(n + 1, 3, "simulation3", conf))(0).getName should be ("datacenter1")
+  }
+  it should "consist of 2 hosts" in {
+    List.tabulate(1)(n => MyDatacenter.create(n + 1, 3, "simulation3", conf))(0).getHostList.size should be (2)
+  }
 
-  // TEST: create hosts
-  @Test @DisplayName("Host Creation")
-  def createHost(): Unit = {
-    log.info("*** TEST: Host Creation ***")
-    val num_host: Int = conf.getInt("simulation3.datacenter1.num_host")
-    assertNotNull(num_host)
-    assertEquals(2, num_host)
-    log.info("Creating hosts...")
-    val hosts: util.List[Host] = MyHost.create(num_host, "datacenter1", "simulation3.datacenter1", conf)
-    assertNotNull(hosts)
-    assertEquals(num_host, hosts.size)
-    val host1Mips: Double = hosts.get(0).getAvailableMips
-    val host1Ram: Double = hosts.get(0).getRam
-    val host2Bw: Long = hosts.get(1).getBw
-    val host2Storage: Long = hosts.get(1).getStorage
-    assertNotNull(host1Mips)
-    assertNotNull(host1Ram)
-    assertNotNull(host2Bw)
-    assertNotNull(host2Storage)
-    log.info(s"host1 available MIPS: $host1Mips (expected 16000.0)")
-    log.info(s"host1 memory: $host1Ram (expected 4096.0)")
-    log.info(s"host2 bandwidth: $host2Bw (expected 10000)")
-    log.info(s"host2 storage: $host2Storage (expected 1000000)")
-    assertEquals(16000.0, host1Mips)
-    assertEquals(4096.0, host1Ram)
-    assertEquals(10000, host2Bw)
-    assertEquals(1000000, host2Storage)
-    hosts.forEach(host => log.info(String.format("host%d successfully created.", host.getId)))
-    log.info("Test successfully completed.")
-  }//end def createHost
+  // TEST 4: create hosts
+  "Hosts" should "have 2 in total" in {
+    conf.getInt("simulation3.datacenter1.num_host") should be (2)
+  }
+  it should "have 6000 MIPS in host1" in {
+    MyHost.create(2, "datacenter1", "simulation3.datacenter1", conf).get(0).getAvailableMips should be (6000.0)
+  }
+  it should "have 2 GB RAM in host1" in {
+    MyHost.create(2, "datacenter1", "simulation3.datacenter1", conf).get(0).getRam should be (2048.0)
+  }
+  it should "have 10 GB bandwidth in host2" in {
+    MyHost.create(2, "datacenter1", "simulation3.datacenter1", conf).get(1).getBw should be (10000)
+  }
+  it should "have 1 TB storage in host2" in {
+    MyHost.create(2, "datacenter1", "simulation3.datacenter1", conf).get(1).getStorage should be (1000000)
+  }
 
-  // TEST: create brokers with VMs and cloudlets
-  @Test @DisplayName("Broker Creation")
-  def createBroker(): Unit = {
-    log.info("*** TEST: Broker Creation ***")
-    log.info("Creating broker...")
-    val num_broker: Int = conf.getInt("simulation3.num_broker")
-    val map_reduce: Boolean = conf.getBoolean("simulation3.map_reduce")
-    assertNotNull(num_broker)
-    assertNotNull(map_reduce)
-    assertEquals(2, num_broker)
-    assertTrue(map_reduce)
-    log.info("Creating VMs...")
-    log.info("Creating cloudlets...")
-    val broker: DatacenterBroker = MyBroker.create(1, 3, "simulation3", map_reduce, conf)
-    assertNotNull(broker)
-    val brokerName: String = broker.getName
-    assertNotNull(brokerName)
-    assertEquals("broker1", brokerName)
-    log.info(String.format("%s successfully created.", brokerName))
-    val vms: util.List[_ <: Vm] = broker.getVmList
-    assertNotNull(vms)
-    assertEquals(3, vms.size)
-    (0 until vms.size).foreach(vm => log.info(String.format("VM #%d successfully created.", vm + 1)))
-    val cloudlets: util.List[_ <: Vm] = broker.getCloudletList
-    assertNotNull(cloudlets)
-    log.info(String.format("%d slave cloudlets successfully created.", cloudlets.size))
-    log.info("Test successfully completed.")
-  }//end def createBroker
+  // TEST 5: create brokers with VMs and cloudlets
+  "Brokers" should "be 2 in total" in {
+    conf.getInt("simulation3.num_broker") should be (2)
+  }
+  it should "have the names broker1 and broker2, respectively" in {
+    val brokers: List[DatacenterBroker] = List.tabulate(2)(b => MyBroker.create(b + 1, 3, "simulation3", map_reduce = true, conf))
+    brokers(0).getName should be ("broker1")
+    brokers(1).getName should be ("broker2")
+  }
+  it should "have 3 VMs and 2 VMs, respectively" in {
+    val brokers: List[DatacenterBroker] = List.tabulate(2)(b => MyBroker.create(b + 1, 3, "simulation3", map_reduce = true, conf))
+    brokers(0).getVmList.size should be (3)
+    brokers(1).getVmList.size should be (2)
+  }
 
-  // TEST: create virtual machines
-  @Test @DisplayName("Virtual Machine Creation")
-  def createVm(): Unit = {
-    log.info("*** TEST: Virtual Machine Creation ***")
-    val num_vm: Int = conf.getInt("simulation3.broker1.num_vm")
-    assertNotNull(num_vm)
-    assertEquals(3, num_vm)
-    log.info("Creating VMs...")
-    val vms: util.List[Vm] = MyVm.create(num_vm, 1, "broker1", "simulation3.broker1", conf)
-    assertNotNull(vms)
-    assertEquals(num_vm, vms.size)
-    val vm1Bw: Long = vms.get(0).getBw
-    val vm1Pes: Int = vms.get(0).getNumberOfPes
-    val vm2Id: Int = vms.get(1).getId
-    val vm2Size: Long = vms.get(1).getSize
-    val vm3Mips: Double = vms.get(2).getMips
-    val vm3Vmm: String = vms.get(2).getVmm
-    assertNotNull(vm1Bw)
-    assertNotNull(vm1Pes)
-    assertNotNull(vm2Id)
-    assertNotNull(vm2Size)
-    assertNotNull(vm3Mips)
-    assertNotNull(vm3Vmm)
-    log.info(s"VM #1 bandwidth: $vm1Bw (expected 1000)")
-    log.info(s"VM #1 number of CPUs: $vm1Pes (expected 1)")
-    log.info(s"VM #2 ID: $vm2Id (expected 2)")
-    log.info(s"VM #2 image size: $vm2Size (expected 10000)")
-    log.info(s"VM #3 MIPS rating: $vm3Mips (expected 1000.0)")
-    log.info(s"VM #3 VM monitor: $vm3Vmm (expected Xen)")
-    assertEquals(1000, vm1Bw)
-    assertEquals(1, vm1Pes)
-    assertEquals(2, vm2Id)
-    assertEquals(10000, vm2Size)
-    assertEquals(1000.0, vm3Mips)
-    assertEquals("Xen", vm3Vmm)
-    vms.forEach(vm => log.info(String.format("VM #%d successfully created.", vm.getId)))
-    log.info("Test successfully completed.")
-  }//end def createVm
+  // TEST 6: create virtual machines
+  "Virtual Machines" should "have 3 in total" in {
+    conf.getInt("simulation3.broker1.num_vm") should be (3)
+  }
+  it should "have 1 GB bandwidth in host1" in {
+    MyVm.create(3, 1, "broker1", "simulation3.broker1", conf).get(0).getBw should be (1000)
+  }
+  it should "have 1 CPU in VM #1" in {
+    MyVm.create(3, 1, "broker1", "simulation3.broker1", conf).get(0).getNumberOfPes should be (1)
+  }
+  it should "have an image size of 10 GB in VM #2" in {
+    MyVm.create(3, 1, "broker1", "simulation3.broker1", conf).get(1).getSize should be (10000)
+  }
+  it should "have 1000 MIPS in VM #3" in {
+    MyVm.create(3, 1, "broker1", "simulation3.broker1", conf).get(2).getMips should be (1000.0)
+  }
+  it should "have the Xen VM monitor in VM #3" in {
+    MyVm.create(3, 1, "broker1", "simulation3.broker1", conf).get(2).getVmm should be ("Xen")
+  }
 
-  // TEST: create cloudlets
-  @Test @DisplayName("Cloudlet Creation")
-  def createCloudlet(): Unit = {
-    log.info("*** TEST: Cloudlet Creation ***")
-    val c: Config = ConfigFactory.load("simulation2")
-    assertNotNull(c)
-    val num_vm: Int = c.getInt("simulation2.broker1.num_vm")
-    val num_cloudlet: Int = c.getInt("simulation2.broker1.num_cloudlet")
-    assertNotNull(num_vm)
-    assertNotNull(num_cloudlet)
-    assertEquals(3, num_vm)
-    assertEquals(15, num_cloudlet)
-    val cloudlets: util.List[Cloudlet] = MyCloudlet.create(num_cloudlet, 0, "broker1", "simulation2.broker1", c)
-    assertNotNull(cloudlets)
-    assertEquals(15, cloudlets.size)
-    val cloudlet1Id: Int = cloudlets.get(14).getCloudletId
-    val cloudlet6Id: Int = cloudlets.get(9).getCloudletId
-    val cloudlet15Id: Int = cloudlets.get(0).getCloudletId
-    assertNotNull(cloudlet1Id)
-    assertNotNull(cloudlet6Id)
-    assertNotNull(cloudlet15Id)
-    log.info(s"Cloudlet 1 ID: $cloudlet1Id (expected 1)")
-    log.info(s"Cloudlet 6 ID: $cloudlet6Id (expected 6)")
-    log.info(s"Cloudlet 15 ID: $cloudlet15Id (expected 15)")
-    assertEquals(1, cloudlet1Id)
-    assertEquals(6, cloudlet6Id)
-    assertEquals(15, cloudlet15Id)
-    log.info(String.format("%d cloudlets successfully created.", cloudlets.size))
-    log.info("Test successfully completed.")
-  }//end def createCloudlet
+  // TEST 7: create cloudlets
+  "Cloudlets" should "be 15 in total" in {
+    ConfigFactory.load("simulation2").getInt("simulation2.broker1.num_cloudlet") should be (15)
+  }
+  it should "have 300 bytes for cloudlet 2" in {
+    MyCloudlet.create(15, 0, "broker1", "simulation2.broker1", ConfigFactory.load("simulation2")).get(1).getCloudletFileSize should be (300)
+  }
+  it should "have ID 12 for cloudlet 12" in {
+    MyCloudlet.create(15, 0, "broker1", "simulation2.broker1", ConfigFactory.load("simulation2")).get(11).getCloudletId should be (12)
+  }
+  it should "need 1 CPU for cloudlet 15" in {
+    MyCloudlet.create(15, 0, "broker1", "simulation2.broker1", ConfigFactory.load("simulation2")).get(14).getNumberOfPes should be (1)
+  }
 
-  // TEST: MapReduce cloudlets and display results
-  @Test @DisplayName("MapReduce")
-  def mapReduce(): Unit = {
-    log.info("*** TEST: MapReduce ***")
-    log.info("Creating datacenter...")
-    log.info("Creating hosts...")
-    val dc: Datacenter = MyDatacenter.create(1, 3, "simulation3", conf)
-    assertNotNull(dc)
-    val name: String = dc.getName
-    assertNotNull(name)
-    assertEquals("datacenter1", name)
-    log.info(s"$name successfully created.")
-    val hosts: util.List[_ <: Host] = dc.getHostList
-    assertNotNull(hosts)
-    assertEquals(2, hosts.size)
-    (0 until hosts.size).foreach(host => log.info(String.format("host%d successfully created.", host + 1)))
-    log.info("Creating broker...")
-    log.info("Creating VMs...")
-    log.info("Creating cloudlets...")
-    val map_reduce: Boolean = conf.getBoolean("simulation3.map_reduce")
-    assertNotNull(map_reduce)
-    assertTrue(map_reduce)
-    val broker: DatacenterBroker = MyBroker.create(2, 3, "simulation3", map_reduce, conf)
-    assertNotNull(broker)
-    val brokerName: String = broker.getName
-    assertNotNull(brokerName)
-    assertEquals("broker2", brokerName)
-    log.info(String.format("%s successfully created.", brokerName))
-    log.info("Beginning simulation...")
+  // TEST 8: MapReduce cloudlets and display results
+  "MapReduce" should "have datacenter1" in {
+    MyDatacenter.create(1, 3, "simulation3", conf).getName should be ("datacenter1")
+  }
+  it should "have host1 and host2" in {
+    MyDatacenter.create(1, 3, "simulation3", conf).getHostList.size should be (2)
+  }
+  it should "run successfully with broker2" in {
+    val broker: DatacenterBroker = MyBroker.create(2, 3, "simulation3", map_reduce = true, conf)
+    broker.getName should be ("broker2")
     CloudSim.startSimulation()
-    log.info("Reducing mapped cloudlets...\n")
-    MyCloudlet.reducer(3, brokerName, broker.getCloudletReceivedList)
-    log.info("Test successfully completed.")
-  }//end def mapReduce
+    val cloudlets: util.List[_ <: Cloudlet] = broker.getCloudletReceivedList
+    cloudlets.size should be > 15
+    MyCloudlet.reducer(3, "broker2", cloudlets)
+  }
 }//end class SimulationTests
